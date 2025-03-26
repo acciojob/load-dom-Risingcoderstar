ea1019362776dfa8827a22789d42d19acf0ecc88
Risingcoderstar <94625582+Risingcoderstar@users.noreply.github.com>
2025-03-26 17:14:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+
+const t=document.querySelector("body");
+document.addEventListener("DOMContentLoaded",()=>{
+const x=document.createElement("div");
+x.innerHTML="<h1>DOM load success</h1>";
+t.appendChild(x);
+
+})
